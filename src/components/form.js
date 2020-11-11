571e9d44ee94538f5df9a8e0276449d170adf08b
Terry <19tp01@gmail.com>
2020-11-11 12:02:20 +0800

    final changes

--- src/components/form.js
+++ src/components/form.js
@@ -1,25 +1,41 @@
 import React, { Fragment, useState } from "react"
 import { useForm } from "react-hook-form"
 import axios from "axios"
-import { Link, useTranslation } from "gatsby-plugin-react-i18next"
+import { Link, useI18next, useTranslation } from "gatsby-plugin-react-i18next"
+import Checkmark from "../icons/checkmark.svg"
 
 const Form = props => {
   const { t } = useTranslation()
+  const { languages, changeLanguage, language } = useI18next()
+
   const { register, handleSubmit, errors, reset } = useForm()
+  const [message, setMessage] = useState("")
 
   const onSubmit = data => {
-    console.log(data)
     // Creates new FormData object and adds keys
 
+    const formObj = new FormData()
+    Object.keys(data).forEach(key => formObj.append(key, data[key]))
     try {
       axios({
         method: "post",
         url:
           "https://script.google.com/macros/s/AKfycby4B9swblgUkD4bFNuRDDibPKnOFWPLh8GSFosN1Xl7A8sKT_U/exec",
-        data: data,
+        data: formObj,
         headers: { "Content-Type": "multipart/form-data" },
       })
       console.log("submit success")
+      if (language === "en") {
+        setMessage(
+          `${data.songTitle} by ${data.artist}? Of course we know that one. We'll get to it as soon as we can.`
+        )
+      } else {
+        setMessage(
+          `${data.artist} 的 ${data.songTitle}？我們當然會那首歌。我們經快為您表演。`
+        )
+      }
+
+      reset()
     } catch (err) {
       console.log(err)
     }
@@ -27,6 +43,14 @@ const Form = props => {
 
   return (
     <Fragment>
+      {message && (
+        <div className="align-center alert">
+          <Checkmark style={{ width: "30px", height: "30px" }} />
+          <h2 style={{ padding: "5px 0 10px" }}>{t("FORM.SUBMITTED")}</h2>
+          <p>{message}</p>
+        </div>
+      )}
+
       <form onSubmit={handleSubmit(onSubmit)}>
         <label for="songTitle">
           {t("FORM.SONG_TITLE")}
